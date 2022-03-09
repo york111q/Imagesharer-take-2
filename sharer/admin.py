8b49c5f96dd4bc6f3b86542cc6e5192598fa362a
york111q <york111q@gmail.com>
2022-03-09 20:19:54 +0100

    Admin configuration

--- sharer/admin.py
+++ sharer/admin.py
@@ -1,22 +1,32 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
-from .models import AccountTier, UserImage, ImageThumb, Thumbnail, UserAccountTier
+from .models import (AccountTier, UserImage, ImageThumb, Thumbnail,
+                     UserAccountTier)
 
 
 # Register your models here.
+class ImageThumbAdmin(admin.ModelAdmin):
+    list_display = ['image_object', 'size', 'code', 'expiry_date']
+    list_filter = ['image_object', 'expiry_date']
+    ordering = ['image_object', 'size', 'expiry_date']
+
+
+class ImageThumbInline(admin.TabularInline):
+    model = ImageThumb
+    readonly_fields = ['image_object', 'image_file', 'size', 'code',
+                       'expiry_date']
+    max_num = 0
+
+
+class UserImageAdmin(admin.ModelAdmin):
+    list_display = ['user', 'image', 'code']
+    list_filter = ['user']
+    ordering = ['user']
+    inlines = [ImageThumbInline]
+
+
 admin.site.register(AccountTier)
-admin.site.register(UserImage)
-admin.site.register(ImageThumb)
+admin.site.register(UserImage, UserImageAdmin)
+admin.site.register(ImageThumb, ImageThumbAdmin)
 admin.site.register(Thumbnail)
 admin.site.register(UserAccountTier)
-
-
-class MyUserAdmin(UserAdmin):
-    list_display = ('username', 'account_type', 'is_admin')
-    search_fields = ('username', 'account_type')
-    readonly_fields = ('date_joined', 'last_login')
-
-    filter_horizontal = ()
-    list_filter = ()
-    fieldsets = ()
